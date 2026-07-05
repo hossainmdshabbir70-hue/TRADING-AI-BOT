1bc9738c16fadd193fca4c26ec3b1d17c314be0b
hossainmdshabbir70-hue <hossainmdshabbir70@gmail.com>
2026-07-06 00:31:52 +0600

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from telegram.ext import (
 )
 
 # 🔑 তোমার BotFather token এখানে বসাবে
-TOKEN = ""8999370933:AAGMh6xONhfRHC4-JoEfhst4h3lEECZ6zUE
+TOKEN = "TOKEN = "8999370933:AAGMh6xONhfRHC4-JoEfhst4h31EECZ6zUE"
 
 # 📊 OTC Market list
 MARKETS = [
